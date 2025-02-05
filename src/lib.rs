73665decdfac02fbbcdf5b712b605c4e891b3d2b
Joe Mastrianni <jmastrianni13@gmail.com>
2025-02-05 14:45:00 -0500

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -81,3 +81,79 @@ impl Worker {
         };
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::mpsc;
+    use std::sync::Arc;
+    use std::sync::Mutex;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_threadpool_creation() {
+        let pool = ThreadPool::new(4);
+        assert_eq!(pool.workers.len(), 4);
+        assert!(pool.sender.is_some());
+    }
+
+    #[test]
+    #[should_panic(expected = "size > 0")]
+    fn test_new_threadpool_0_size() {
+        ThreadPool::new(0);
+    }
+
+    #[test]
+    fn test_job_execution() {
+        let pool = ThreadPool::new(2);
+        let (tx, rx) = mpsc::channel();
+
+        for _ in 0..4 {
+            let tx = tx.clone();
+            pool.execute(move || {
+                tx.send(1).unwrap();
+            });
+        }
+
+        let sum: i32 = rx.iter().take(4).sum();
+        assert_eq!(sum, 4);
+    }
+
+    #[test]
+    fn test_threadpool_shutdown() {
+        let pool = ThreadPool::new(2);
+        let (tx, rx) = mpsc::channel();
+
+        for i in 0..2 {
+            let tx = tx.clone();
+            pool.execute(move || {
+                println!("job {} started", i);
+                thread::sleep(Duration::from_millis(10));
+                tx.send(()).unwrap();
+                println!("job {} completed", i);
+            });
+        }
+
+        drop(pool); // test Drop trait implementation
+
+        for _ in 0..2 {
+            rx.recv().unwrap();
+        }
+
+        assert_eq!(rx.try_iter().count(), 0); // assert all jobs completed before shutdown
+    }
+
+    #[test]
+    fn test_worker_shutdown_message() {
+        let (tx, rx) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(rx));
+        let worker = Worker::new(1, Arc::clone(&receiver));
+
+        drop(tx);
+
+        thread::sleep(Duration::from_millis(10));
+
+        assert!(worker.w_thread.unwrap().is_finished());
+    }
+}
