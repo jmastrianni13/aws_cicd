b172f5c712bc8e934a25ad652df96436e0e2d70a
Joe Mastrianni <jmastrianni13@gmail.com>
2025-08-19 17:58:07 -0400

    added development tools to instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "simple_server" {
   ami           = "ami-083522e25d3e4d203"
   instance_type = "t4g.micro"
 
+  user_date = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum groupinstall "Development Tools" -y
+  EOF
+
   tags = {
     Name = "EC2 Instance Simple Server"
   }
