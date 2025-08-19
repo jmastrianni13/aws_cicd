87912da4d4477f1226c679c8e714d3a98e69880f
Joe Mastrianni <jmastrianni13@gmail.com>
2025-08-19 17:48:43 -0400

    remove key_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "simple_server" {
   ami           = "ami-083522e25d3e4d203"
   instance_type = "t4g.micro"
 
-  key_name = "ec2_simple_server_key_pair"
-
   tags = {
     Name = "EC2 Instance Simple Server"
   }
