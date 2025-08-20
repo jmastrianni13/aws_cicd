fa883b295021143afebaf950b407b285d35b191f
Joe Mastrianni <jmastrianni13@gmail.com>
2025-08-20 00:02:21 -0400

    added ec2 for client

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,24 @@ resource "aws_instance" "simple_server" {
   }
 }
 
+data "aws_instance" "simple_server" {
+  instance_id = "i-04002736142d2b9e6"
+}
+
+resource "aws_instance" "simple_client" {
+  ami           = "ami-083522e25d3e4d203"
+  instance_type = "t4g.nano"
+  key_name      = "simple_client"
+  subnet_id     = data.aws_instance.simple_server.subnet_id
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum groupinstall "Development Tools" -y
+  EOF
+
+  tags = {
+    Name = "EC2 Instance Simple Client"
+  }
+}
+
