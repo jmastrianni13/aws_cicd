39e465008baaaea53ba476e28976a07a62a2a3c3
Joe Mastrianni <jmastrianni13@gmail.com>
2025-09-25 11:32:59 -0400

    wip add sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "simple_sg" {
+  name        = "simle-server-client-sg"
+  description = "experimental sg for server/client running in same vcp"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_instance" "simple_server" {
   ami           = "ami-083522e25d3e4d203"
   instance_type = "t4g.micro"
   key_name      = "simple_server"
+
+  vpc_security_group_ids = [aws_security_group.simple_sg.id]
 
   user_data = <<-EOF
   #!/bin/bash
@@ -27,6 +54,8 @@ resource "aws_instance" "simple_client" {
   instance_type = "t4g.nano"
   key_name      = "simple_client"
   subnet_id     = data.aws_instance.simple_server.subnet_id
+
+  vpc_security_group_ids = [aws_security_group.simple_sg.id]
 
   user_data = <<-EOF
   #!/bin/bash
