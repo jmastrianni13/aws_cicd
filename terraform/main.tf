8781cd5897b02c8876e426154eef3d16f84698a9
Joe Mastrianni <jmastrianni13@gmail.com>
2025-09-28 23:28:09 -0400

    update ingress port range

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "simple_sg" {
   ingress {
     self        = true
     from_port   = 0
-    to_port     = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = [aws_vpc.simple_vpc.cidr_block]
   }
