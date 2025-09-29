cfa326433d4412f727022f3089ffa663368d6efd
Joe Mastrianni <jmastrianni13@gmail.com>
2025-09-28 22:38:47 -0400

    add more network components to allow ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ resource "aws_vpc" "simple_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "simple_gateway" {
+  vpc_id = aws_vpc.simple_vpc.id
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
@@ -15,6 +19,19 @@ resource "aws_subnet" "simple_subnet" {
   availability_zone       = data.aws_availability_zones.available.names[0]
   cidr_block              = cidrsubnet(aws_vpc.simple_vpc.cidr_block, 8, 1)
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "simple_route_table" {
+  vpc_id = aws_vpc.simple_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.simple_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "simple_route_table_association" {
+  subnet_id      = aws_subnet.simple_subnet.id
+  route_table_id = aws_route_table.simple_route_table.id
 }
 
 resource "aws_security_group" "simple_sg" {
